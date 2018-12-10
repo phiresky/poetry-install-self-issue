69381c21a7a56eddf94241ed17c4397abe1a28c0
Robin Ruede <robin.ruede@philips.com>
2018-12-10 10:05:16 +0100

    example

---
+++ a/b.py
@@ -0,0 +1,3 @@
+import c.x
+
+print(c.x.test)
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="cooli",
+    version="0.1",
+    url="",
+    packages=find_packages(),
+    install_requires=[]
+)
